8ebe0a4f04d86da71145c3485d25570ab54f40c2
Artem Karlov <ar.karlov@gmail.com>
2023-11-14 00:03:28 +0100

    Update binary search

--- 01_introduction_to_algorithms/01_binary_search.ts
+++ 01_introduction_to_algorithms/01_binary_search.ts
@@ -4,6 +4,8 @@
  * @param {number[]} array The sorted array in which the search is performed..
  * @param {number} searchElement The value to locate in the array.
  * @returns {number} The index of the searchElement in the array, or -1 if not found.
+ *
+ * @timecomplexity The time complexity of binary search is O(log n).
  */
 function binarySearch<T extends number>(array: T[], searchElement: T): number {
   let left = 0;
@@ -25,7 +27,7 @@ function binarySearch<T extends number>(array: T[], searchElement: T): number {
   return -1;
 }
 
-const testArray = [2, 3, 4, 5, 6, 7, 8];
+const binarySearchTestArray = [1, 3, 0, 1, 9, 4, 8, 7, 5, 2];
 
-console.log(binarySearch(testArray, 3)); // 1
-console.log(binarySearch(testArray, 10)); // -1
+console.log(binarySearch(binarySearchTestArray, 3)); // 1
+console.log(binarySearch(binarySearchTestArray, 10)); // -1
